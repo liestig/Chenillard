7f50ef99048282872d2483edb8a2cf19ffbadeb2
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-01-26 10:33:05 +0100

    First push: LED lighting sequentially

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TP_Microcontr_leur_Mouisse_Mabile_Tiguercha.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TP_Microcontr_leur_Mouisse_Mabile_Tiguercha.X.production.hex
