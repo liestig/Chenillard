490f9ee1ddc8bbcdb423d1bb617fc3026a4a3c08
Liès TIGUERCHA <lies.tiguercha@decathlon.com>
2024-02-09 09:33:20 +0100

    TP Buzzer PWM

--- TP_Buzzer_PWM.X/nbproject/Makefile-impl.mk
+++ TP_Buzzer_PWM.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=TP_Chenillard
+PROJECTNAME=TP_Buzzer_PWM.X
 
 # Active Configuration
 DEFAULTCONF=default
--- TP_Chenillard/nbproject/Makefile-variables.mk
+++ TP_Chenillard/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=TP_Microcontr_leur_Mouisse_Mabile_Tiguercha.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/TP_Microcontr_leur_Mouisse_Mabile_Tiguercha.X.production.hex
+CND_ARTIFACT_NAME_default=TP_Chenillard.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TP_Chenillard.production.hex
